3f74bf9e8bf334a34d89c89f57f049ab4468056a
ignacio <ignadario97@gmail.com>
2019-07-04 12:03:29 -0300

    controller banda, barra lateral y pantalla para crear banda

--- Codigo Fuente/src/Views/Perfil/modificar.php
+++ Codigo Fuente/src/Views/Perfil/modificar.php
@@ -55,8 +55,73 @@
 
 
     </div>
-    <div class="container-fluid">
-        <div class="m-5 p-3 border border-dark rounded-0">
+    <div class="container-fluid d-flex">
+    <div class="d-flex w-25 mt-5 flex-column">
+    <div class="accordion" id="accordionExample">
+  <div class="card">
+    <div class="card-header" id="headingOne">
+      <h5 class="mb-0">
+        <button class="btn btn-link text-dark" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+          Suscripciones
+        </button>
+      </h5>
+    </div>
+
+    <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordionExample">
+      <div class="card-body">
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-header" id="headingTwo">
+      <h5 class="mb-0">
+        <button class="btn btn-link collapsed text-dark" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+          Difusiones
+        </button>
+      </h5>
+    </div>
+    <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
+      <div class="card-body">
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-header" id="headingThree">
+      <h5 class="mb-0">
+        <button class="btn btn-link collapsed text-dark" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+          Medios de pago
+        </button>
+      </h5>
+    </div>
+    <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
+      <div class="card-body">
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-header" id="headingFour">
+      <h5 class="mb-0">
+        <button class="btn btn-link collapse show text-dark" type="button" data-toggle="collapse" data-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+          Bandas
+        </button>
+      </h5>
+    </div>
+    <div id="collapseFour" class="collapse" aria-labelledby="headingFour" data-parent="#accordionExample">
+      <div class="card-body d-flex flex-column">
+      <div class="mb-3" style="cursor:pointer;">
+      <?php echo "<a href ='".getBaseAddress() ."Banda/crear'>"; ?>
+      Crear banda
+      </div>
+      </a>
+      <div style="cursor:pointer;">
+      Mis bandas
+      </div>
+      </div>
+    </div>
+  </div>
+</div>
+    </div>
+        <div class="m-5 w-75 p-3 border border-dark rounded-0">
             <h3 class="text-left mb-4">Información Personal</h3>
             <form method="POST" action=" <?php echo getBaseAddress() . "Perfil/modificarUsuario"; ?>">
                 <div class="d-flex justify-content-center align-items-center">
