102fd440526591e3a6ba6e14be4226ddaecdbf10
ignacio <ignadario97@gmail.com>
2019-07-04 12:10:38 -0300

    boton en barra lateral faltante

--- Codigo Fuente/src/Views/Banda/crear.php
+++ Codigo Fuente/src/Views/Banda/crear.php
@@ -117,6 +117,19 @@
       </div>
     </div>
   </div>
+  <div class="card">
+    <div class="card-header" id="headingFive">
+      <h5 class="mb-0">
+        <button class="btn btn-link collapsed text-dark" type="button" data-toggle="collapse" data-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
+          Perfil
+        </button>
+      </h5>
+    </div>
+    <div id="collapseFive" class="collapse" aria-labelledby="headingFive" data-parent="#accordionExample">
+      <div class="card-body">
+      </div>
+    </div>
+  </div>
 </div>
     </div>
         <div class="m-5 w-75 p-3 border border-dark rounded-0">
