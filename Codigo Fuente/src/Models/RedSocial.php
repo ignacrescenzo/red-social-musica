f5637726c085dbf8fe571550cd4f16b7f6503f04
Tomás Joaquín Pais <tomas.j.pais@gmail.com>
2019-07-06 19:31:50 -0300

    Modifico modelos de Banda y RedSocial

--- Codigo Fuente/src/Models/RedSocial.php
+++ Codigo Fuente/src/Models/RedSocial.php
@@ -83,4 +83,24 @@ class RedSocial extends Model {
     public function setBandaId($bandaId) {
         $this->bandaId = $bandaId;
     }
+
+    public function guardarRedSocialBanda()
+    {
+        $array = [
+            "BandaId" => $this->getBandaId(),
+            "Link" => $this->getLink()
+        ];
+
+        $id = $this->insert($array);
+
+        if($id)
+            $this->setId($id);
+
+        return $id;
+    }
+
+    public function existeRedSocial()
+    {
+        return $this->pageRows(0, 1, "Link LIKE '%" . $this->getLink() . "%'");
+    }
 }
